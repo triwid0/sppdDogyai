ff3acf100178a35948996f7c834a33e4ab10d173
unknown <findseven317@gmail.com>
2023-09-25 14:32:37 +0700

    edit masterlokasi

--- sppd/forms/sppd.py
+++ sppd/forms/sppd.py
@@ -8,13 +8,12 @@ class MasterTahun(forms.ModelForm):
 		model = MasterTahun
 		fields = ('tahun',)
 
-
 class MasterTahun(forms.ModelForm):
 	class Meta:
 		model = MasterLokasi
 		fields = ('kd_lokasi',)
 		fields = ('urai',)
-
+		
 class MasterJabatan(forms.ModelForm):
 	class Meta:
 		model = MasterJabatan
