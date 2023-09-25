10e7242d43cfa0ab7b3ae19d3fbac32bc2846e8b
unknown <findseven317@gmail.com>
2023-09-25 14:11:33 +0700

    tambah masterlokasi

--- sppd/models/sppd.py
+++ sppd/models/sppd.py
@@ -10,12 +10,11 @@ class MasterTahun(models.Model):
     tahun = models.CharField(max_length=4)
     status = models.BooleanField(max_length=1, default=False)
 
-<<<<<<< HEAD
 class MasterLokasi(models.Model):
     kd_lokasi = models.CharField(max_length=225)
     urai = models.TextField(max_length=225)
-=======
+
 class MasterJabatan(models.Model):
     jabatan = models.CharField(max_length=225)
     status = models.BooleanField(max_length=1, default=False)
->>>>>>> dab51d3061e792ba290ebc3ffd7cdec7fd7758b7
+
--- sppd/views/__init__.py
+++ sppd/views/__init__.py
@@ -1,9 +1,7 @@
 from .user import *
 from .tahun import *
-<<<<<<< HEAD
 from .lokasi import *
 from .dashboard import *
-=======
 from .dashboard import *
 from .jabatan import *
->>>>>>> dab51d3061e792ba290ebc3ffd7cdec7fd7758b7
+
